07308587a0eb433ccd1f355abc855f0cb0405182
lutek <lutek.valhalla@gmail.com>
2013-06-26 11:42:26 -0500

    Creando el AJAX

--- trama/components/com_jumi/files/crear_proyecto/form_crear_proyecto.php
+++ trama/components/com_jumi/files/crear_proyecto/form_crear_proyecto.php
@@ -73,33 +73,47 @@ $document->addScript($pathJumi.'js/jquery.MultiFile.js');
 	<br />
 	
 	<label for="linkYt2">Enlace Youtube 2:</label> 
-	<input type="text" id="linkYt2" class="validate[custom[yt]]" name="youtubeLink2"> <br />
+	<input type="text" id="linkYt2" class="validate[custom[yt]]" name="youtubeLink2">
+	<br />
 	
 	<label for="linkYt3">Enlace Youtube 3:</label> 
-	<input type="text" id="linkYt3" class="validate[custom[yt]]" name="youtubeLink3"> <br />
+	<input type="text" id="linkYt3" class="validate[custom[yt]]" name="youtubeLink3"> 
+	<br />
 	
-	<label for="linkYt4">Enlace Youtube 4:</label> <input type="text" id="linkYt4" class="validate[custom[yt]]" name="youtubeLink4"> <br />
+	<label for="linkYt4">Enlace Youtube 4:</label> 
+	<input type="text" id="linkYt4" class="validate[custom[yt]]" name="youtubeLink4"> 
+	<br />
 	
-	<label for="linkYt5">Enlace Youtube 5:</label> <input type="text" id="linkYt5" class="validate[custom[yt]]" name="youtubeLink5"> <br />
+	<label for="linkYt5">Enlace Youtube 5:</label> 
+	<input type="text" id="linkYt5" class="validate[custom[yt]]" name="youtubeLink5"> 
+	<br />
 	
-	<br /> Audios promocionales (solo links de soundcloud): <br /> 
+	<br /> Audios promocionales (solo links de soundcloud): 
+	<br /> 
+	
 	<label for="linkSc1">Enlace Soundcloud 1:</label> 
-	<input type="text" id="linkSc1" class="validate[custom[sc]]" name="soundCloudLink1"> <br />
+	<input type="text" id="linkSc1" class="validate[custom[sc]]" name="soundCloudLink1"> 
+	<br />
 	
 	<label for="linkSc">Enlace Soundcloud 2:</label> 
-	<input type="text" id="linkSc2" class="validate[custom[sc]]" name="soundCloudLink2"> <br />
+	<input type="text" id="linkSc2" class="validate[custom[sc]]" name="soundCloudLink2"> 
+	<br />
 	
 	<label for="linkSc3">Enlace Soundcloud 3:</label> 
-	<input type="text" id="linkSc3" class="validate[custom[sc]]" name="soundCloudLink3"> <br />
+	<input type="text" id="linkSc3" class="validate[custom[sc]]" name="soundCloudLink3"> 
+	<br />
 	
 	<label for="linkSc4">Enlace Soundcloud 4:</label> 
-	<input type="text" id="linkSc4" class="validate[custom[sc]]" name="soundCloudLink4"> <br />
+	<input type="text" id="linkSc4" class="validate[custom[sc]]" name="soundCloudLink4"> 
+	<br />
 	
 	<label for="linkSc5">Enlace Soundcloud 5:</label> 
-	<input type="text" id="linkSc5" class="validate[custom[sc]]" name="soundCloudLink5"> <br />
+	<input type="text" id="linkSc5" class="validate[custom[sc]]" name="soundCloudLink5"> 
+	<br />
 	
 	<label for="fotos">Fotos m&aacute;ximo 10 m&iacute;nimo una*:</label> 
-	<input class="multi validate[required]" id="fotos" accept="gif|jpg|x-png" type="file" maxlength="10" name="photo"> <br /> 
+	<input class="multi validate[required]" id="fotos" accept="gif|jpg|x-png" type="file" maxlength="10" name="photo"> 
+	<br /> 
 	
 	<label for="descProy">Descripci&oacute;n del proyecto*:</label> <br />
 	<textarea name="description" id="descProy" class="validate[required]" cols="60" rows="5"></textarea>
@@ -110,41 +124,62 @@ $document->addScript($pathJumi.'js/jquery.MultiFile.js');
 	
 	<br /> 
 	<label for="direccion">Direcci&oacute;n del recinto*: </label> 
-	<input type="text" class="validate[required]" id="direccion" name="showground" maxlength="100"> <br> 
+	<input type="text" class="validate[required]" id="direccion" name="showground" maxlength="100"> 
+	<br> 
 	
-	<label for="plantilla">Cargar la plantilla de Excel con el Business Case*:</label> <input type="file" class="validate[required]" id="plantilla" name="businessCase"> <br />
-	<label for="presupuesto">Presupuesto del proyecto*:</label> <input type="number" class="validate[required]"	id="presupuesto" name="budget"> <br /> 
+	<label for="plantilla">Cargar la plantilla de Excel con el Business Case*:</label> 
+	<input type="file" class="validate[required]" id="plantilla" name="businessCase"> 
+	<br />
 	
-	<br /> Precios de salida del proyecto*: <br /> <br /> 
+	<label for="presupuesto">Presupuesto del proyecto*:</label> 
+	<input type="number" class="validate[required]"	id="presupuesto" name="budget"> 
+	<br /> 
+	
+	<br /> Precios de salida del proyecto*: 
+	<br />
+	<br /> 
 	
 	<label for="seccion">Secci&oacute;n*:</label>
-	<input type="text" id="seccion" class="validate[required,custom[onlyLetterNumber]]" name="section"> <br />
+	<input type="text" id="seccion" class="validate[required,custom[onlyLetterNumber]]" name="section"> 
+	<br />
 	
 	<label for="unidad">Precio unidad*:</label> 
-	<input type="number" id="unidad" class="validate[required,custom[onlyNumberSp]]" name="unitSale" step="any"> <br> 
+	<input type="number" id="unidad" class="validate[required,custom[onlyNumberSp]]" name="unitSale" step="any"> 
+	<br> 
 	
 	<label for="inventario">Inventario(Cantidad)*:</label>
-	<input type="number" id="inventario" class="validate[required,custom[onlyNumberSp]]" name="capacity"> <br />
+	<input type="number" id="inventario" class="validate[required,custom[onlyNumberSp]]" name="capacity"> 
+	<br />
+	<br />
+	 
+	<span id="writeroot"></span> 
+	<input type="button" onclick="moreFields()" value="Agregar campos" /> <br /> 
+	<br /> 
 	
-	<br /> <span id="writeroot"></span> <input type="button"
-		onclick="moreFields()" value="Agregar campos" /> <br /> <br /> <label
-		for="potenicales">Ingresos potenciales del proyecto*:</label> <input
-		type="number" id="potenciales"
-		class="validate[required,custom[onlyNumberSp]]"
-		name="revenuePotential" step="any"> <br> <label for="equilibrio">Punto
-		de equilibrio*:</label> <input type="number" id="equilibrio"
-		class="validate[required,custom[onlyNumberSp]]" name="breakeven"
-		step="any"> <br> <label for="productionStartDate">Fecha inicio
-		producci&oacute;n*:</label> <input type="date"
-		id="productionStartDate" class="validate[required]"
-		name="productionStartDate"> <br> <label for="productionEndDate">Fecha
-		fin de producci&oacute;n*:</label> <input type="date"
-		id="productionEndDate" class="validate[required]"
-		name="productionEndDate"> <br> <label for="premiereStartDate">Fecha
-		inicio presentaci&oacute;n*:</label> <input type="date"
-		id="premiereStartDate" class="validate[required]"
-		name="premiereStartDate"> <br> <label for="premiereEndDate">Fecha fin
-		de presentaci&oacute;n*:</label> <input type="date"
-		id="premiereEndDate" class="validate[required]" name="premiereEndDate">
-	<br> <br> <input type="submit" value="Enviar">
+	<label for="potenicales">Ingresos potenciales del proyecto*:</label> 
+	<input type="number" id="potenciales" class="validate[required,custom[onlyNumberSp]]" name="revenuePotential" step="any"> 
+	<br>
+	
+	<label for="equilibrio">Punto de equilibrio*:</label>
+	<input type="number" id="equilibrio" class="validate[required,custom[onlyNumberSp]]" name="breakeven" step="any"> 
+	<br>
+	
+	<label for="productionStartDate">Fecha inicio producci&oacute;n*:</label> 
+	<input type="date" id="productionStartDate" class="validate[required]" name="productionStartDate"> 
+	<br>
+	
+	<label for="productionEndDate">Fecha fin de producci&oacute;n*:</label> 
+	<input type="date" id="productionEndDate" class="validate[required]" name="productionEndDate"> 
+	<br> 
+	
+	<label for="premiereStartDate">Fecha inicio presentaci&oacute;n*:</label> 
+	<input type="date" id="premiereStartDate" class="validate[required]" name="premiereStartDate"> 
+	<br> 
+	
+	<label for="premiereEndDate">Fecha fin de presentaci&oacute;n*:</label> 
+	<input type="date" id="premiereEndDate" class="validate[required]" name="premiereEndDate">
+	<br /> 
+	<br/> 
+	
+	<input type="submit" value="Enviar">
 </form>
