459f898729118bb343d3b32f6221b885fb6cdc0e
Ricardo <ricardolyon@gmail.com>
2013-07-08 14:13:47 -0500

    correccion css resultados de busqueda, acrualizacion detalle de proyectos

--- trama/components/com_jumi/files/ver_proyecto/solicitud_participar.php
+++ trama/components/com_jumi/files/ver_proyecto/solicitud_participar.php
@@ -1,43 +1,69 @@
 <?php
-defined('_JEXEC') OR defined('_VALID_MOS') OR die( "Direct Access Is Not Allowed" );
-
-$usuario = JFactory::getUser();
+defined('_JEXEC') OR defined('_VALID_MOS') OR die("Direct Access Is Not Allowed");
 ?>
 <script>
-	jQuery(document).ready(function(){		
-		jQuery("#form2").validationEngine();
+	jQuery("#solicitud").submit(function() {
 		
-		jQuery("#enviar").click(function (){
-			var form = jQuery("#form2")[0];
-			var total = form.length;
-			
-			var user = <?php echo $usuario->id; ?>;;
-	
-			}
-			
-			jQuery("#form2").submit();
-		});
+		console.log(jQuery("#mensaje").val().length);
+		// if (jQuery("#mensaje").val().length >= 3) {
+			// jQuery("span").text("Validated...").show();
+			// return true;
+		// }
+		// jQuery("span").text("Not valid!").show().fadeOut(4000);
+		// return false;
+	});
 </script>
 
-<?php 
-function participar() {
+<?php
+function participar($json) {
+	JHTML::_('behavior.modal');
 	$usuario = JFactory::getUser();
-	$html = '<p>'.
-			'<a class="button" href="#" data-rokbox="" data-rokbox-element="div.modalcontact">'.JText::_('SOLICITA_PARTICIPAR').'</a>'.
-			'</p>'.
-			'<div class="esconder" style="position: absolute; left: -1000px;">'.
-			'<div class="modalcontact">'.
-			'<div class="inside" style="width: 300px;">'.
-			'<form id="" action="" method="post">'.
-			'<input id="nombre" type="text" name="remitente" value="'. $usuario->name .'" disabled />'.
-			'<label for="mensaje">Mensaje</label>'.
-			'<textarea name="mensaje"></textarea>'.
-			'<input type="hidden" name="receptor" /><input type="submit" value="Enviar" />'.
-			'</form>'.
-			'</div>'.
-			'</div>'.
-			'</div>';
-	// <a href="#" data-rokbox="" data-rokbox-element="div.popup_contact"></a>
-	return $html;
+	$producer = JFactory::getUser($json -> userId);
+	
+	$script = '<script>'.
+			'jQuery("#solicitud").submit(function() {'.
+				
+				'console.log(jQuery("#mensaje").val().length);'.
+				// if (jQuery("#mensaje").val().length >= 3) {
+					// jQuery("span").text("Validated...").show();
+					// return true;
+				// }
+				// jQuery("span").text("Not valid!").show().fadeOut(4000);
+				// return false;
+			'});'.
+			'</script>';
+
+	$html = $script.'<p>' . '<a class="button" href="#" data-rokbox="" data-rokbox-element="div.modalcontact">' .
+			 JText::_('SOLICITA_PARTICIPAR') . '</a>' .
+			 '</p>' . 
+			 '<div class="esconder" style="position: absolute; left: -1000px;">' . 
+			 '<div class="modalcontact">' . 
+			 '<div class="inside" style="width: 300px;">' . 
+			 '<form id="solicitud" action="" method="post">' . 
+			 '<label for="nombre">'.JText::_('USUARIO').'</label>'.
+			 '<input id="nombre" type="text" name="remitente" value="' . $usuario -> name . '" disabled />' . 
+			 '<label for="mensaje">'.JText::_('MENSAJE').'</label>' . 
+			 '<textarea id="mensaje" name="mensaje"></textarea>' . 
+			 '<input type="hidden" name="proyId" value="' . $producer -> id . '" />' . 
+			 '<input type="submit" value="Enviar" />' . 
+			 '</form>' . 
+			 '</div>' . 
+			 '</div>' . 
+			 '</div>';
+	// $html = '<a href="#esconder" class="modal" rel="{size: {x: 50%, y: 500}}" id="modalLink1">Click here to see this interesting page</a>'.
+			 // '<div id="esconder" style="display: none;">' . 
+			 // '<div class="modalcontact">' . 
+			 // '<div class="inside" style="width: 300px;">' . 
+			 // '<form id="solicitud" action="" method="post">' . 
+			 // '<input id="nombre" type="text" name="remitente" value="' . $usuario -> name . '" disabled />' . 
+			 // '<label for="mensaje">Mensaje</label>' . 
+			 // '<textarea name="mensaje"></textarea>' . 
+			 // '<input type="hidden" name="proyId" value="' . $producer -> id . '" />' . 
+			 // '<input type="submit" value="Enviar" />' . 
+			 // '</form>' . 
+			 // '</div>' . 
+			 // '</div>' . 
+			 // '</div>';
+		return $html;
 }
 ?>
--- trama/components/com_jumi/files/ver_proyecto/ver_proyecto.php
+++ trama/components/com_jumi/files/ver_proyecto/ver_proyecto.php
@@ -16,24 +16,13 @@
 <script type="text/javascript" src="components/com_jumi/files/ver_proyecto/js/jquery.nivo.slider.js"></script>
 <?php
 	$url = MIDDLE.PUERTO.'/trama-middleware/rest/project/get/'.$proyecto;
-//	$homepage = file_get_contents($url);
+	$homepage = file_get_contents($url);
 
-$homepage = '{"id":1,"name":"Slava snow show","showground":"Av. Chapultepec y Av. CuauhtÃ©moc, Del. CuauhtÃ©moc,  MÃ©xico,  DF  06700","inclosure":"Centro Cultural Telmex","breakeven":100000,"budget":200000,"revenuePotential":500000,"description":"Desde su primera apariciÃ³n en MÃ©xico en 2006, este show ha cautivado a propios y extraÃ±os, ya que con mÃ­mica y trucos bÃ¡sicos generan admiraciÃ³n en todo el pÃºblico. Los payasos consiguen que los asistentes disfruten de un mÃ¡gico mundo alejado de los problemas, en medio de la frÃ­a nieve. \r\nLa compaÃ±Ã­a fundada por el ruso Slava Polunin y que tiene entre sus filas a los mejores payasos del mundo se renueva para esta temporada y ofrecerÃ¡ el mejor espectÃ¡culo, siempre lleno de sorpresas para propios y extraÃ±os.\r\n\r\nEl Centro Cultural Telmex 2 recibirÃ¡ en esta ocasiÃ³n a los payasos que trasladan al pÃºblico de un estado de Ã¡nimo a otro, haciendo alusiÃ³n a los caprichos del clima. \r\n\r\nA decir de su fundador, Slavaâ€™s tiene tantas descripciones como hay miembros de la audiencia. â€œSnowshow es la belleza de un solo copo de nieve que cae suavemente y cae en su hombro. Snowshow es el sonido de una risa incontrolable en adultos no hay nada en particular. Snowshow es la alegrÃ­a de creer que todo es posible, Snowshow es la tristeza de un adiÃ³s de una plataforma de la estaciÃ³n de tren. Snowshow es la constataciÃ³n de que la vida es realmente maravillosaâ€.\r\nGanador del â€œOliver Awardâ€ como El Mejor show en Londres, el espectÃ¡culo de Slava ha causado furor mundialmente y una vez mÃ¡s lo harÃ¡ en MÃ©xico a partir del 22 de mayo en el Centro Cultural Telmex 2. ","cast":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu ante aliquam, posuere massa at, pulvinar lectus. Cras condimentum ornare sapien, quis mattis odio tincidunt at. Cras faucibus ipsum in adipiscing tincidunt. Vivamus auctor, augue eu interdum blandit, massa nunc ultrices purus, vitae rhoncus nibh diam eget ligula. Suspendisse scelerisque nulla sodales euismod sodales. Fusce id augue nec nisi malesuada tempus. Duis imperdiet metus sed sapien mollis, nec commodo massa faucibus. In enim libero, rutrum a commodo eu, aliquam a felis. Aenean eget urna ac lorem porttitor luctus. Maecenas pharetra nibh ac nunc pulvinar, quis cursus elit egestas.","fundStartDate":null,"fundEndDate":null,"productionStartDate":"2013-07-04","premiereStartDate":"2013-08-04","premiereEndDate":"2013-09-06","timeCreated":1372973266545,"subcategory":15,"status":0,"type":"PROJECT","userId":379,"projectBanner":{"projectBannerId":1,"name":"1372973267118_0.png"},"projectAvatar":{"projectAvatarId":1,"name":"1372973267856_0.png"},"projectBusinessCase":{"projectBusinessCaseId":1,"name":"1372973268132"},"projectUnitSales":[{"id":1,"section":"1A","unitSale":500,"capacity":3,"projectId":1}],"projectYoutubes":[{"id":5,"url":"https://vimeo.com/13850491","projectId":1},{"id":4,"url":"https://vimeo.com/13850491","projectId":1},{"id":3,"url":"http://www.youtube.com/watch?v=sL_7d1UFZ6s","projectId":1},{"id":2,"url":"http://www.youtube.com/watch?v=d9bfiNBtxcA","projectId":1},{"id":1,"url":"http://www.youtube.com/watch?v=fpUY7e-k4Jo","projectId":1}],"projectSoundclouds":[{"id":5,"url":"","projectId":1},{"id":4,"url":"","projectId":1},{"id":3,"url":"","projectId":1},{"id":2,"url":"","projectId":1},{"id":1,"url":"","projectId":1}],"projectPhotos":[{"id":1,"name":"1372973269979_0.png","projectId":1},{"id":2,"name":"1372973275368_5.png","projectId":1},{"id":3,"name":"1372973273644_2.png","projectId":1},{"id":4,"name":"1372973275593_6.png","projectId":1},{"id":5,"name":"1372973275981_7.png","projectId":1},{"id":6,"name":"1372973272232_1.png","projectId":1},{"id":7,"name":"1372973275167_4.png","projectId":1},{"id":8,"name":"1372973275062_3.png","projectId":1}]}';
+//$homepage = '{"id":1,"name":"Slava snow show","showground":"Av. Chapultepec y Av. CuauhtÃ©moc, Del. CuauhtÃ©moc,  MÃ©xico,  DF  06700","inclosure":"Centro Cultural Telmex","breakeven":100000,"budget":200000,"revenuePotential":500000,"description":"Desde su primera apariciÃ³n en MÃ©xico en 2006, este show ha cautivado a propios y extraÃ±os, ya que con mÃ­mica y trucos bÃ¡sicos generan admiraciÃ³n en todo el pÃºblico. Los payasos consiguen que los asistentes disfruten de un mÃ¡gico mundo alejado de los problemas, en medio de la frÃ­a nieve. \r\nLa compaÃ±Ã­a fundada por el ruso Slava Polunin y que tiene entre sus filas a los mejores payasos del mundo se renueva para esta temporada y ofrecerÃ¡ el mejor espectÃ¡culo, siempre lleno de sorpresas para propios y extraÃ±os.\r\n\r\nEl Centro Cultural Telmex 2 recibirÃ¡ en esta ocasiÃ³n a los payasos que trasladan al pÃºblico de un estado de Ã¡nimo a otro, haciendo alusiÃ³n a los caprichos del clima. \r\n\r\nA decir de su fundador, Slavaâ€™s tiene tantas descripciones como hay miembros de la audiencia. â€œSnowshow es la belleza de un solo copo de nieve que cae suavemente y cae en su hombro. Snowshow es el sonido de una risa incontrolable en adultos no hay nada en particular. Snowshow es la alegrÃ­a de creer que todo es posible, Snowshow es la tristeza de un adiÃ³s de una plataforma de la estaciÃ³n de tren. Snowshow es la constataciÃ³n de que la vida es realmente maravillosaâ€.\r\nGanador del â€œOliver Awardâ€ como El Mejor show en Londres, el espectÃ¡culo de Slava ha causado furor mundialmente y una vez mÃ¡s lo harÃ¡ en MÃ©xico a partir del 22 de mayo en el Centro Cultural Telmex 2. ","cast":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu ante aliquam, posuere massa at, pulvinar lectus. Cras condimentum ornare sapien, quis mattis odio tincidunt at. Cras faucibus ipsum in adipiscing tincidunt. Vivamus auctor, augue eu interdum blandit, massa nunc ultrices purus, vitae rhoncus nibh diam eget ligula. Suspendisse scelerisque nulla sodales euismod sodales. Fusce id augue nec nisi malesuada tempus. Duis imperdiet metus sed sapien mollis, nec commodo massa faucibus. In enim libero, rutrum a commodo eu, aliquam a felis. Aenean eget urna ac lorem porttitor luctus. Maecenas pharetra nibh ac nunc pulvinar, quis cursus elit egestas.","fundStartDate":null,"fundEndDate":null,"productionStartDate":"2013-07-04","premiereStartDate":"2013-08-04","premiereEndDate":"2013-09-06","timeCreated":1372973266545,"subcategory":15,"status":0,"type":"PROJECT","userId":379,"projectBanner":{"projectBannerId":1,"name":"1372973267118_0.png"},"projectAvatar":{"projectAvatarId":1,"name":"1372973267856_0.png"},"projectBusinessCase":{"projectBusinessCaseId":1,"name":"1372973268132"},"projectUnitSales":[{"id":1,"section":"1A","unitSale":500,"capacity":3,"projectId":1}],"projectYoutubes":[{"id":5,"url":"https://vimeo.com/13850491","projectId":1},{"id":4,"url":"https://vimeo.com/13850491","projectId":1},{"id":3,"url":"http://www.youtube.com/watch?v=sL_7d1UFZ6s","projectId":1},{"id":2,"url":"http://www.youtube.com/watch?v=d9bfiNBtxcA","projectId":1},{"id":1,"url":"http://www.youtube.com/watch?v=fpUY7e-k4Jo","projectId":1}],"projectSoundclouds":[{"id":5,"url":"","projectId":1},{"id":4,"url":"","projectId":1},{"id":3,"url":"","projectId":1},{"id":2,"url":"","projectId":1},{"id":1,"url":"","projectId":1}],"projectPhotos":[{"id":1,"name":"1372973269979_0.png","projectId":1},{"id":2,"name":"1372973275368_5.png","projectId":1},{"id":3,"name":"1372973273644_2.png","projectId":1},{"id":4,"name":"1372973275593_6.png","projectId":1},{"id":5,"name":"1372973275981_7.png","projectId":1},{"id":6,"name":"1372973272232_1.png","projectId":1},{"id":7,"name":"1372973275167_4.png","projectId":1},{"id":8,"name":"1372973275062_3.png","projectId":1}]}';
 	$json = json_decode($homepage);
-// var_dump($json);
 
 	$producer = JFactory::getUser($json->userId);
 
-	$urlSubcategoria = MIDDLE.PUERTO.'/trama-middleware/rest/category/subcategories/all';
-	$jsonSubcategoria = file_get_contents($urlSubcategoria);
-	$jsonObjSubcategoria = json_decode($jsonSubcategoria);
-	
-	foreach($jsonObjSubcategoria as $key=>$value) {
-		if($value->id == $json->subcategory) {
-			$nombreSubcategoria = $value->name;
-		}
-	}
-	
 	$db = JFactory::getDbo();
 	
 	$query = $db->getQuery(true);
@@ -48,6 +37,17 @@ $homepage = '{"id":1,"name":"Slava snow show","showground":"Av. Chapultepec y Av
 	$results = $db->loadObjectList();
 
 $tipo = prefijoTipo($json);
+
+function getProySubCatName($data) {
+    $urlSubcategoria = MIDDLE.PUERTO.'/trama-middleware/rest/category/subcategories/all';
+	$subcats = json_decode(file_get_contents($urlSubcategoria));
+	foreach ($subcats as $key => $value) {
+		if($value->id == $data->subcategory) {
+			$nomCat= $value->name;
+		}
+	}
+	return $nomCat;
+}
 
 function prefijoTipo($data) {
 	switch ($data->type) {
@@ -66,7 +66,7 @@ function buttons($data, $user) {
 		$link = 'index.php?option=com_jumi&view=appliction&fileid=9';
 		$proyid = '&proyid='.$data->id;
 		$html = '<div id="buttons">'.
-				'<div><a href="'.$link.$proyid.'">'.JText::_('edit').'</a></div>';
+				'<div><a href="'.$link.$proyid.'">'.JText::_('EDIT').'</a></div>';
 		return $html;
 	}
 }
@@ -196,13 +196,21 @@ function irGrupo($data) {
 	return $html;
 }
 
-function informacionTmpl($data) {
+function encabezado($data, $results) {
+	$html = '<h2>'.$data->name.'</h2>'.
+		'<h4>'.getProySubCatName($data).'</h4>'.
+		'<p>'.$results[0]->nomNombre.' '.$results[0]->nomApellidoPaterno.'</p>';
+	
+	return $html;
+}
+
+function informacionTmpl($data, $results) {
  	require_once 'solicitud_participar.php';
 	$html = '<div id="izquierdaDesc" class="gantry-width-33 gantry-width-block">'.
 			'<div class="gantry-width-spacer">'.
 			avatar($data).
 			'<div class="gantry-width-spacer flotado">'.
-	  		participar().
+	  		participar($data).
 			'</div>'.
 			'<div class="gantry-width-spacer flotado">'.
 			irGrupo($data).
@@ -211,6 +219,7 @@ function informacionTmpl($data) {
 			'</div>'.
 			'<div id="derechaDesc" class="gantry-width-66 gantry-width-block">'.
 			'<div class="gantry-width-spacer">'.
+			encabezado($data, $results).
 			$data->description.
 			'</div>'.
 			'</div>'.
@@ -278,9 +287,7 @@ function fechas($data, $tipo) {
 				<div class="content-banner">
 					<img src="<?php echo MIDDLE.BANNER.'/'.$json->projectBanner->name ?>" />
 					<div class="info-banner">
-						<h2><?php echo $json->name;?></h2>
-						<h4><?php echo $results[0]->nomNombre." ".$results[0]->nomApellidoPaterno;?></h4>
-						<p><?php echo $nombreSubcategoria;?></p>
+						<?php echo encabezado($json, $results); ?>
 					</div>
 				</div>
 			</div>
@@ -316,6 +323,7 @@ function fechas($data, $tipo) {
 			</div>
 			<div id="finanzas" class="ver_proyecto">
 				<h3>Finanzas</h3>
+				<?php echo encabezado($json, $results); ?>
 				<?php echo finanzas($json); ?>
 				<?php echo tablaFinanzas($json); ?>
 				<?php echo fechas($json, $tipo); ?>
@@ -324,7 +332,7 @@ function fechas($data, $tipo) {
 			<div id="info" class="ver_proyecto">
 				<h3>Informacion</h3>
 				<div class="detalleDescripcion">
-					<?php echo informacionTmpl($json); ?>
+					<?php echo informacionTmpl($json, $results); ?>
 				</div>
 				<a class="cerrar">cerrar</a>
 			</div>
@@ -347,5 +355,5 @@ function fechas($data, $tipo) {
     </script>
 
 <?php 
- //var_dump($json);
+// var_dump($json);
  ?>
