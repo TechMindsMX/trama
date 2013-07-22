496d2a7cec87277c5e5dd55c411735cc87b30b2c
csantana <jcsantana2989@hotmail.com>
2013-07-22 12:30:46 -0500

    Se agrego redireccionamiento a los formularios

--- trama/components/com_jumi/files/busqueda_perfil_x/buscar_perfil_x.php
+++ trama/components/com_jumi/files/busqueda_perfil_x/buscar_perfil_x.php
@@ -1,7 +1,7 @@
 <?php
 defined('_JEXEC') or die( 'Restricted access' );
 
-$path = 'images/fotoperfil/';
+$path = 'images/fotoPerfil/';
 $pathJumi = Juri::base().'components/com_jumi/files';
 
 $tabla = "perfilx_respuestas";
@@ -48,7 +48,7 @@ function agrupacionIds($tabla, $valor, $campo){
 		} else {
 			
 			$allDone =& JFactory::getApplication();
-			$allDone->redirect('index.php', 'No se encontro ningun resultado' );
+			$allDone->redirect($_SERVER['HTTP_REFERER'], 'No se encontro ningun resultado' );
 		
 		}
 				
@@ -74,7 +74,7 @@ foreach ($var as $key => $value) {
 
 if(empty($valor)) {
 	$allDone =& JFactory::getApplication();
-	$allDone->redirect('index.php', 'No se selecciono ninguna busqueda' );
+	$allDone->redirect($_SERVER['HTTP_REFERER'], 'No se selecciono ninguna busqueda' );
 }
 
 $agrupacionIds = agrupacionIds($tabla, $valor, $campo);
