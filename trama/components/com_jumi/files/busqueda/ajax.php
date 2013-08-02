b589b8c5715686f322d091edf7b921c879d5c62b
csantana <jcsantana2989@hotmail.com>
2013-08-02 12:51:37 -0500

    Funcionalidad para poder compartir proyectos en el muro de jomsocial

--- trama/components/com_jumi/files/busqueda/ajax.php
+++ trama/components/com_jumi/files/busqueda/ajax.php
@@ -46,6 +46,48 @@ switch ($fun) {
 		$url = "http://192.168.0.122:7070/sepomex-middleware/rest/sepomex/get/".$_POST["cp"];
 		echo file_get_contents($url);
 		break;
+		
+	case 3:
+		
+		$userId = $_POST['userId'];
+		$projectId = $_POST['projectId'];
+		$linkProyecto = $_POST['linkProyecto'];
+		$nomUser = $_POST['nomUser'];
+		$nomProyecto = $_POST['nomProyecto'];
+		
+		$bd = new mysqli('localhost', 'root', '', 'development_j25');
+		
+		$queryShared = 'SELECT * FROM c3rn2_community_activities WHERE actor = '.$userId.' && proyId = '.$projectId;
+		$resultShared = $bd->query($queryShared);
+		
+		if ($resultShared->num_rows == 0) {
+			
+			$queryMaxId = 'SELECT MAX(id) AS id from c3rn2_community_activities';
+			$resultado = $bd->query($queryMaxId);
+			
+			$objMaxId = $resultado->fetch_object();
+			$objMaxId->id = $objMaxId->id + 1;
+			
+			$frase = $nomUser.' a compartido el proyecto '.$nomProyecto.', '.$linkProyecto;
+			$fecha = date("Y-m-d H:i:s");
+			
+			$query_proy = 'INSERT INTO `c3rn2_community_activities` ';
+			$query_proy .= '(`id`, `actor`, `target`, `title`, `content`, `app`, `verb`, `cid`, `groupid`, `eventid`, `group_access`, `event_access`, `created`, `access`, `params`, `points`, `archived`, `location`, `latitude`, `longitude`, `comment_id`, `comment_type`, `like_id`, `like_type`, `actors`, `proyId`)';
+			$query_proy .= 'VALUES ("NULL", "'.$userId.'", "'.$userId.'", "'.$frase.'", "", "profile", "", "'.$userId.'", "0", "0", "0", "0", "'.$fecha.'", "20", "", "1", "0", "", "255", "255", "'.$objMaxId->id.'", "profile.status", "'.$objMaxId->id.'", "profile.status", "", "'.$projectId.'")';
+			
+			$bd->query($query_proy);
+			
+			$respuesta['shared'] = false;
+			
+		} else {
+			
+			$respuesta['shared'] = true;
+			$respuesta['name'] = $nomUser;
+			
+		}
+
+		echo json_encode($respuesta);
+		break;
 	
 	default:
 		echo 'error';
