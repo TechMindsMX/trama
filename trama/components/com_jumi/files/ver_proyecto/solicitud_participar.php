b3eeb011c8341f1f57b3b5351db67cbcf07efb8c
Ricardo <ricardolyon@gmail.com>
2013-08-05 11:35:59 -0500

    elimina lineas comentadas

--- trama/components/com_jumi/files/ver_proyecto/solicitud_participar.php
+++ trama/components/com_jumi/files/ver_proyecto/solicitud_participar.php
@@ -14,32 +14,6 @@ function participar($json) {
 	$onclick = CMessaging::getPopup($json->userId);
 	$html = '<a class="button" onclick="'.$onclick.'" href="#">'.JText::_('SOLICITA_PARTICIPAR').'</a>';	
 	
-	// JHTML::_('behavior.modal');
-	// $usuario = JFactory::getUser();
-	// $producer = JFactory::getUser($json -> userId);
-// 	
-	// $accion = MIDDLE.'';
-// 	
-	// $script = '';
-// 	
-	// $html = $script.'<p>' . '<a class="button" href="#" data-rokbox="" data-rokbox-element="div.modalcontact">' .
-			 // JText::_('SOLICITA_PARTICIPAR') . '</a>' .
-			 // '</p>' . 
-			 // '<div class="esconder">' . 
-			 // '<div class="modalcontact">' . 
-			 // '<div class="inside" style="width: 300px;">' . 
-			 // '<form id="solicitud" action="/sourcetree/trama/test.php" method="post">' . 
-			 // '<label for="nombre">'.JText::_('USUARIO').'</label>'.
-			 // '<input id="nombre" type="text" value="' . $usuario -> name . '" readonly />' . 
-			 // '<label for="mensaje">'.JText::_('MENSAJE').'</label>' . 
-			 // '<textarea id="mensaje" name="mensaje"></textarea>' . 
-			 // '<input type="hidden" name="proyId" value="' . $producer -> id . '" />' . 
-			 // '<input type="hidden" name="remitente" value="' . $usuario -> id . '" />' . 
-			 // '<input type="submit" value="Enviar" id="enviar"/>' . 
-			 // '</form>' . 
-			 // '</div>' . 
-			 // '</div>' . 
-			 // '</div>';
 		return $html;
 }
 ?>
