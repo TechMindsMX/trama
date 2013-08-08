7bb8ba2f80da3cfc72550612224b7b1771c7aedf
Ricardo <ricardolyon@gmail.com>
2013-08-08 16:58:08 -0500

    cambios controllers y models

--- trama/administrator/components/com_tramagremios/views/tramagremios/tmpl/default.php
+++ trama/administrator/components/com_tramagremios/views/tramagremios/tmpl/default.php
@@ -9,6 +9,9 @@ JHtml::_('behavior.tooltip');
     <table class="adminlist">
         <thead>
 			<tr>
+				<th width="1%">
+					<input type="checkbox" name="checkall-toggle" value="" title="<?php echo JText::_('JGLOBAL_CHECK_ALL'); ?>" onclick="Joomla.checkAll(this)" />
+				</th>
 		        <th width="5">
 		        	<?php echo JText::_('COM_TRAMAGREMIOS_TRAMAGREMIOS_HEADING_ID'); ?>
 		        </th>
@@ -34,6 +37,9 @@ JHtml::_('behavior.tooltip');
 			foreach($this->items as $i => $item):
 			 ?>
 				<tr class="row<?php echo $i % 2; ?>">
+					<td>
+						<?php echo JHtml::_('grid.id', $i, $item->id); ?>
+					</td>
 					<td>
 						<?php echo $item->users_id; ?>
 					</td>
--- trama/administrator/components/com_tramagremios/views/tramagremios/view.html.php
+++ trama/administrator/components/com_tramagremios/views/tramagremios/view.html.php
@@ -37,7 +37,7 @@ class TramaGremiosViewTramaGremios extends JView
         }
         protected function addToolBar() 
         {
-                JToolBarHelper::title(JText::_('COM_TRAMAGREMIOS_MANAGER_TRAMAGREMIOSS'));
+                JToolBarHelper::title(JText::_('COM_TRAMAGREMIOS_MANAGER_TRAMAGREMIOS'));
                 JToolBarHelper::custom('block.change', 'block.png', 'block_f2.png', 'Block', true);
                 JToolBarHelper::custom('block.change', 'unblock.png', 'unblock_f2.png', 'Unblock', true);
         }
