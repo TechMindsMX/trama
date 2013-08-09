b9d46d8ced776aa40d4192092203403d87f3ba40
Ricardo <ricardolyon@gmail.com>
2013-08-09 12:35:44 -0500

    control de acceso en componente trama-gremios

--- trama/administrator/components/com_tramagremios/controllers/tramagremios.php
+++ trama/administrator/components/com_tramagremios/controllers/tramagremios.php
@@ -1,15 +1,16 @@
 <?php
-defined('_JEXEC') or die;
+defined('_JEXEC') or die ;
 
 jimport('joomla.application.component.controlleradmin');
 
 class TramaGremiosControllerTramaGremios extends JControllerAdmin {
-	
-	public function __construct($config = array())
-	{
+
+	public function __construct($config = array()) {
 		parent::__construct($config);
 
-		$this->registerTask('block',		'changeBlock');
-		$this->registerTask('unblock',		'changeBlock');
+		$this -> registerTask('block', 'changeBlock');
+		$this -> registerTask('unblock', 'changeBlock');
 	}
+
+
 }
