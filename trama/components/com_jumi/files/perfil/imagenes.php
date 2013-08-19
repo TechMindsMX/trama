02bb2d00793166bb2023e49bc5f4c2d6a468292d
lutek <lutek.valhalla@gmail.com>
2013-08-19 16:40:54 -0500

    se agrego la clase manejoImagenes

---
+++ trama/components/com_jumi/files/perfil/imagenes.php
@@ -0,0 +1,45 @@
+<?php
+class manejoImagenes {
+	function cargar_imagen($tipo, $usuario){
+		$validaciones = ( ($tipo === 'image/jpeg') || ($tipo === 'image/gif') || ($tipo === 'image/png') );
+		if($validaciones && getimagesize($_FILES["daGr_Foto"]["tmp_name"])){
+			move_uploaded_file($_FILES["daGr_Foto"]["tmp_name"],"images/fotoPerfil/" .$usuario.".jpg");
+			$this->resize("images/fotoPerfil/".$usuario.".jpg", $usuario.".jpg", 400, 300);
+		}else{
+			echo 'no es imagen o el archivo esta corrupto <br />';
+		} 
+		
+	}
+	
+	function resize($ruta,$nombre, $max_ancho, $max_alto){
+		$rutaImagenOriginal = $ruta;
+				
+		$img_original = imagecreatefromjpeg($rutaImagenOriginal);
+		
+		list($ancho,$alto) = getimagesize($rutaImagenOriginal);
+
+		$x_ratio = $max_ancho / $ancho;
+		$y_ratio = $max_alto / $alto;
+
+		if( ($ancho <= $max_ancho) && ($alto <= $max_alto) ){
+			$ancho_final = $ancho;
+			$alto_final = $alto;
+		}elseif (($x_ratio * $alto) < $max_alto){
+			$alto_final = ceil($x_ratio * $alto);
+			$ancho_final = $max_ancho;
+		}else{
+			$ancho_final = ceil($y_ratio * $ancho);
+			$alto_final = $max_alto;
+		}
+		
+		$tmp=imagecreatetruecolor($ancho_final,$alto_final);
+		
+		imagecopyresampled($tmp,$img_original,0,0,0,0,$ancho_final, $alto_final,$ancho,$alto);
+		imagedestroy($img_original);
+		
+		$calidad=95;
+		imagejpeg($tmp,$ruta,$calidad);
+	}
+}
+
+?>
--- trama/components/com_jumi/files/perfil/js/misjs.js
+++ trama/components/com_jumi/files/perfil/js/misjs.js
@@ -54,7 +54,7 @@ function copiarDomCon() {
 
 function datosxCP() {
 	jQuery('input[name$="perfil_codigoPostal_idcodigoPostal"]').change(function () {
-		console.log(this);			
+					
 		var request = $.ajax({
 			url:"components/com_jumi/files/busqueda/ajax.php",
 			data: {
@@ -65,12 +65,10 @@ function datosxCP() {
 		});
 	
 		request.done(function(result){
-			var select_colonias = jQuery('select[name$="perfil_colonias_idcolonias"]');
-			var select_edos = jQuery('select[name$="perfil_estado_idestado"]');
-			
-			
 			var obj = eval('('+result+')');
 			var colonias = obj.dAsenta;
+			var select_colonias = jQuery('select[name$="perfil_colonias_idcolonias"]');
+			var select_edos = jQuery('select[name$="perfil_estado_idestado"]');
 			
 			jQuery('option', select_colonias).remove();
 			jQuery('option', select_edos).remove();
@@ -78,7 +76,10 @@ function datosxCP() {
 			jQuery.each(colonias, function (key, value){
 				select_colonias.append(new Option(value, value));
 			});
+			
 			jQuery('input[name$="perfil_delegacion_iddelegacion"]').val(obj.dMnpio);
+			
+			
 			select_edos.append(new Option(obj.dEstado, obj.dEstado));
 		});
 	
