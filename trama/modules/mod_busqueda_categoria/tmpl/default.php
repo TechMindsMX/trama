08c05e0c6ee885a62b36322f1bcb4c1f6755a733
Ricardo <ricardolyon@gmail.com>
2013-09-02 10:30:27 -0500

    cambio en la forma como carga el modulo de busqueda por categorias

--- trama/modules/mod_busqueda_categoria/tmpl/default.php
+++ trama/modules/mod_busqueda_categoria/tmpl/default.php
@@ -2,22 +2,25 @@
 defined( '_JEXEC' ) or die( 'Restricted access' );
 $document = JFactory::getDocument();
 
+$document->addScript('templates/rt_hexeris/js/jquery-1.9.1.js');
 $document->addScript('modules/mod_busqueda_categoria/js/jquery.chained.js');
+$document->addStyleSheet('modules/mod_busqueda_categoria/css/busq_cat.css');
 $scriptJS = 'jQuery(function() {
 	jQuery("#selectSubCat").chained("#selectCat");	
 });';
+$document->addScriptDeclaration($scriptJS);
 
-$tipoPP = isset($_GET['typeId']) ? '&typeId='.$_GET['typeId'] : '';
+$input = JFactory::getApplication()->input;
+$tipoPP = $input->get('typeId',null,'int');
 
 $accion= 'index.php?option=com_jumi&view=application&fileid=8'.$tipoPP;
 
-$document->addScriptDeclaration($scriptJS);
-
 $opcionesSubCat = '';
 ?>
-<form action="<?php echo $accion; ?>" method="post"> 
+<div class="busq_cat">
+<form action="<?php echo $accion; ?>" method="post" target="_parent"> 
 	<select id="selectCat" name="categoria">
-		<option value="">Seleccione una categoría</option>
+		<option value=""><?php echo JText::_('SELECCIONE').JText::_('CATEGORIA'); ?></option>
 	<?php		
 	foreach ( $categoria as $key => $value ) {
 		echo '<option value="'.$value->id.'">'.$value->name.'</option>';
@@ -27,10 +30,10 @@ $opcionesSubCat = '';
 	</select>
 	
 	<select id="selectSubCat" name="subcategoria">
-			<option value="all">Todas</option>
+			<option value="all"><?php echo JText::_('TODAS'); ?></option>
 	<?php
 	foreach ( $opcionesPadre as $valor ) {
-		$opcionesSubCat .= '<option class="'.$valor.'" value="all">Todas las subcategorias</opcion>';
+		$opcionesSubCat .= '<option class="'.$valor.'" value="all">'. JText::_('TODAS_SUBCATS') .'</opcion>';
 	}
 	foreach ( $subCategorias as $key => $value ) {
 		$opcionesSubCat .= '<option class="'.$value->father.'" value="'.$value->id.'">'.$value->name.'</option>';
@@ -42,4 +45,4 @@ $opcionesSubCat = '';
 	
 	<input type="submit" value="Buscar" >
 </form>
-		
+</div>
