255d90394bbf91386101b5dc0cf135830bbc80f9
erikjbb <erikjbb@gmail.com>
2013-09-12 11:20:31 -0500

    alerta de avatar

--- trama/components/com_jumi/files/crear_proyecto/js/mas.js
+++ trama/components/com_jumi/files/crear_proyecto/js/mas.js
@@ -194,10 +194,10 @@ function loadImage2(input1) {
     }
 
     function imageLoaded() {
-		if(img.width > 600 || img.height > 600 || img.width < 360 || img.height < 360 || peso  > 4096000){
+		if(img.width > 1024 || img.height > 1024 || img.width < 360 || img.height < 360 || peso  > 4096000){
 			
 			
-		alert ("Solo se aceptan imagenes de resolución entre 1920x1200 y 800x600, y con un peso no mayor a 4 mb, su imagen no sera subida. ");
+		alert ("Solo se aceptan imagenes de resolución entre 360x360 y 1024x1024, y con un peso no mayor a 4 mb, su imagen no sera subida. ");
 		$fileupload = $('#'+input1.id);  
 		$fileupload.replaceWith($fileupload.clone(true)); 
 		$('#'+input1.id).val(""); 
