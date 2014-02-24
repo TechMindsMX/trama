7ae6aeb2088dd77c0bce1ecf91137eb6d1224cf0
erikjbb <erikjbb@gmail.com>
2014-02-24 14:06:13 -0600

    avances escritorio

--- trama/components/com_jumi/files/escritorio/escritorio.php
+++ trama/components/com_jumi/files/escritorio/escritorio.php
@@ -77,7 +77,7 @@ $promedio = UserData::scoreUser($userMiddleId->idJoomla);
 					
 					<article class="ac-medium">
 						
-						<div class="escritorio table table-striped" width="100%" frame="box" rules="all" style="text-align: center">
+						<div class="escritorio" width="100%" frame="box" rules="all" style="text-align: center">
 							<div class="clase-tr">
 								<div class="clase-th"><?php echo JText::_('LABEL_NAME');?></div>
 								<div class="clase-th"><?php echo JText::_('LABEL_CREACION');?></div>
@@ -168,7 +168,7 @@ $promedio = UserData::scoreUser($userMiddleId->idJoomla);
 					<input id="ac-3a" name="accordion-2" type="radio" />
 					<label for="ac-3a"><?php echo JText::_('LABEL_PRODUCTOS');?></label>
 					<article class="ac-large">
-						<div class="escritorio table table-striped" width="100%" frame="box" rules="all" style="text-align: center;">
+						<div class="escritorio" width="100%" frame="box" rules="all" style="text-align: center;">
 							<div class="clase-tr">
 								<div class="clase-th"><?php echo JText::_('LABEL_NAME');?></div>
 								<div class="clase-th"><?php echo JText::_('LABEL_CREACION');?></div>
@@ -205,7 +205,7 @@ $promedio = UserData::scoreUser($userMiddleId->idJoomla);
 											echo '<div class="clase-td"><a href="'.$value->viewUrl.'" >'.$value->name.'</a></div>';
 											echo '<div class="clase-td">'.date('d-M-Y',$fecha).'</div>';
 											echo '<div class="clase-td">'.JHTML::tooltip($statusName->tooltipText,$statusName->tooltipTitle,'',$statusName->fullName).'</div>';
-											echo '<div>
+											echo '<div class="clase-td">
 													<a class="button" href="index.php?option=com_community&view=groups&task=viewgroup&groupid='.$groupId->id.'">
 														ir
 													</a>
@@ -255,7 +255,7 @@ $promedio = UserData::scoreUser($userMiddleId->idJoomla);
 					<input id="ac-4a" name="accordion-2" type="radio" />
 					<label for="ac-4a"><?php echo JText::_('LABEL_REPERTORIO');?></label>
 					<article class="ac-small">
-						<div class="escritorio table table-striped" width="100%" frame="box" rules="all" style="text-align: center;">
+						<div class="escritorio" width="100%" frame="box" rules="all" style="text-align: center;">
 							<div class="clase-tr">
 								<div class="clase-th"><?php echo JText::_('LABEL_NAME');?></div>
 								<div class="clase-th">
@@ -283,7 +283,7 @@ $promedio = UserData::scoreUser($userMiddleId->idJoomla);
                     <input id="ac-5a" name="accordion-2" type="radio" />
 					<label for="ac-5a"><?php echo JText::_('LABEL_SUSPENDIDOS');?></label>
 					<article class="ac-large">
-						<div class="escritorio table table-striped" width="100%" frame="box" rules="all" style="text-align: center;">
+						<div class="escritorio" width="100%" frame="box" rules="all" style="text-align: center;">
 							<div class="clase-tr">
 								<div class="clase-th"><?php echo JText::_('LABEL_NAME');?></div>
 								<div class="clase-th"><?php echo JText::_('LABEL_CREACION');?></div>
@@ -306,7 +306,7 @@ $promedio = UserData::scoreUser($userMiddleId->idJoomla);
 										echo '	<div class="clase-td"><a href="'.$value->viewUrl.'" >'.$value->name.'</a></div>';
 										echo '	<div class="clase-td">'.date('d-M-Y',$fecha).'</div>';
 										echo '<div class="clase-td">
-													<a class="button" href="index.php?option=com_community&view=groups&task=viewgroup&groupid='.$groupId->id.'">
+													<a class="button" href="index.php?option=com_community&view=groups&task=viewgroup&groupid='.@$groupId->id.'">
 														ir
 													</a>
 												  </div>';
